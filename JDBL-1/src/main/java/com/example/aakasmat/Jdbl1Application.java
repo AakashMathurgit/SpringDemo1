e98095def26ffc598b5979ee57c3c9f3a300e788
aakas <aakas@LAPTOP-EL12JM7C>
2024-11-09 22:24:46 +0530

    Testing git

---
+++ JDBL-1/src/main/java/com/example/aakasmat/Jdbl1Application.java
@@ -0,0 +1,13 @@
+package com.example.aakasmat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jdbl1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jdbl1Application.class, args);
+	}
+
+}
---
+++ JDBL-1/src/test/java/com/example/aakasmat/Jdbl1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aakasmat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jdbl1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
